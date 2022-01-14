c9f545248d7eae8b0c8e813c2a757aab9e97a24e
JhonVerdugo <>
2022-01-14 01:59:37 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+const d = document
+
+// Activador de Service Worker, encargado de la PWA (Progressive Web Apps)
+// Comentar este código en caso de modificar elementos hasta haberlos completado, si los cambios no se reflejas elimine los datos de navegación que tenga de la web
+/*if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('./sw.js')
+  .then(reg=>console.log('Success',reg))
+  .catch(err=>console.warn('Error',err))
+}*/
+
+// Generador de categorías
+const $main = d.querySelector('main')
+const getHTML = (options) => {
+  let {url,success,error} = options
+  const xhr = new XMLHttpRequest()
+  xhr.addEventListener('readystatechange',e=>{
+    if(xhr.readyState !==4) return
+    if(xhr.status >= 200 && xhr.status < 300){
+      let html = xhr.responseText
+      success(html)
+    } else {
+      let message = xhr.statusText || 'Error'
+      error(`Error ${xhr.status}: ${message}`)
+    }
+  })
+  
+  xhr.open('GET',url)
+  xhr.setRequestHeader('Content-type','text/html; charset=utf-8')
+  xhr.send()
+}
+d.addEventListener('DOMContentLoaded',e=>{
+  // Contenido que carga por defecto de una categoría, dejar comentado si no quiere cargar contenido por defecto.
+  getHTML({
+    url: 'assets/home.html',
+    success: (html)=>$main.innerHTML = html,
+    error: (err)=>$main.innerHTML = `<h1>${err}</h1>`
+  })
+})
+d.addEventListener('click',e=>{
+  // Botón del menú en teléfonos.
+	if(e.target.matches('#showMenu')){
+    const menu = d.querySelector('.header__nav')
+    menu.classList.toggle('active')
+		showMenu.classList.toggle('fa-bars')
+		showMenu.classList.toggle('fa-times')
+	}
+  // Botones generadores de categorías.
+  if(e.target.matches('.header__link')){
+    e.preventDefault()
+    getHTML({
+      url: e.target.href,
+      success: (html)=>$main.innerHTML = html,
+      error: (err)=>$main.innerHTML = `<h1>${err}</h1>`
+    })
+  }
+})
---
+++ sw.js
@@ -0,0 +1,60 @@
+// Service Worker PWA (Progressive Web Apps)
+const CACHE_NAME="cache_v1",
+  urlsToCache=[
+    "./",
+    "./index.html",
+    "./css/normalize.css",
+    "./css/style.css",
+    "./js/script.js",
+    "./assets/header.html",
+    "./assets/economy.html",
+    "./assets/music.html",
+    "./assets/moderation.html",
+    "./assets/footer.html",
+    "./assets/include.js",
+    "./img/logo.webp",
+    "./img/logo-80w.webp",
+    "./img/favicon-256w.webp",
+    "./img/favicon-512w.webp",
+    "./img/favicon-1024w.png"
+  ]
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache).then(() => self.skipWaiting())
+      })
+      .catch((err) => console.log("Falló registro de cache", err))
+  )
+})
+
+self.addEventListener("activate", (e) => {
+  const cacheWhitelist = [CACHE_NAME]
+
+  e.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName)
+            }
+          })
+        )
+      })
+      .then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) {
+        return res
+      }
+      return fetch(e.request)
+    })
+  )
+})
